1eb16cca1e98edd0cfadf2cf914c18228b6aadc1
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2025-10-18 11:43:41 +0600

    added all folders

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
+import CategoryNews from "../pages/CategoryNews";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+        {
+            path: "",
+            element: <Home></Home>
+        },
+        {
+            path: "/category/:id",
+            element: <CategoryNews></CategoryNews>
+        }
+    ]
+  },
+]);
+
+export default router;
