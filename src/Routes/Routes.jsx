3b27b6193a16a263f309f8c360b098787baf3dd7
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2025-10-19 12:05:56 +0600

    login and register

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
 import CategoryNews from "../pages/CategoryNews";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AuthLayout from "../layout/AuthLayout";
 
 
 const router = createBrowserRouter([
@@ -17,9 +20,23 @@ const router = createBrowserRouter([
             path: "/category/:id",
             element: <CategoryNews></CategoryNews>,
             loader: ()=> fetch('/news.json')
+        },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout></AuthLayout>,
+    children: [
+        {
+            path: '/auth/login',
+            element: <Login></Login>,
+        },
+        {
+            path: '/auth/register',
+            element: <Register></Register>,
         }
     ]
-  },
+  }
 ]);
 
 export default router;
